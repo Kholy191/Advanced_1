d11583f78a576c17541b4e314e833757a1585932
Ahmed El-Kholy <a.adel27111@gmail.com>
2025-01-30 08:51:55 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
